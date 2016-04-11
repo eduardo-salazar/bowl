c4ffe3388cd874471d605e066216a5521414a56e
eduardo-salazar <esalazar922@gmail.com>
2016-04-11 22:49:27 +0800

    Fixed spec and model Person

--- controllers/facebook_api.rb
+++ controllers/facebook_api.rb
@@ -1,6 +1,6 @@
 class FacebookAuth
-	ACCESS_TOKEN = ""
 
+	
 	def init (access_token)
 		ACCESS_TOKEN = access_token
 	end
--- models/person.rb
+++ models/person.rb
@@ -1,2 +1,17 @@
 class Person
+	attr_accessible :id, :link, :sex, :name, :img, :mutual_friends
+
+	def to_json
+		JSON({  
+            id: id,
+            img: img,
+            link: link
+            data: {
+              name: name,
+              sex: sex,
+            }
+          },
+         options)
+	end
+
 end

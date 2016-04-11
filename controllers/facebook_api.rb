118fc6043488df8c25026fd37b9787a14ddfd833
eduardo-salazar <esalazar922@gmail.com>
2016-04-11 21:57:55 +0800

    Initial structure

---
+++ controllers/facebook_api.rb
@@ -0,0 +1,12 @@
+class FacebookAuth
+	ACCESS_TOKEN = ""
+
+	def init (access_token)
+		ACCESS_TOKEN = access_token
+	end
+
+	def my_friends
+		
+	end
+
+end
---
+++ spec/facebook_api_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../controllers/facebook_api.rb'
+require 'minites/autorun'
+
+describe 'Test Facebook API'
+	before  |do|
+		
+	end
+end

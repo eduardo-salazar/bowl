2c90856c5b0bc01237b2ef3b745f23e18b8977c4
eduardo-salazar <esalazar922@gmail.com>
2016-04-12 01:32:59 +0800

    Add facebook oauth authentication

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative 'models/person.rb'
 
 # Configuration Sharing Web Service
 class BabyOwlAPI < Sinatra::Base
-  
+  set :public_folder, File.expand_path('../public', __FILE__)
 
   get '/?' do
     'BabyOwl web service is up and running at /api/v1'

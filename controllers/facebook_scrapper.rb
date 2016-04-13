de76458c9fd0b8de4bf3872c95f447c0ceeada01
eduardo-salazar <esalazar922@gmail.com>
2016-04-14 04:32:14 +0800

    fix timeout

--- controllers/facebook_scrapper.rb
+++ controllers/facebook_scrapper.rb
@@ -7,7 +7,7 @@ Capybara.default_driver = :poltergeist
 Capybara.run_server = false
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--load-images=false', '--disk-cache=false'] , timeout: 100)
+  Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--load-images=false', '--disk-cache=false'] , timeout: 100})
 end
 
 class FacebookScapper
